c7ec64e3a6ac505d0bc76d310a7ca65f20a5cb00
Charlie Hengemihle <charliehengemihle@gmail.com>
2022-09-20 13:03:24 -0600

    Dead Roachy :cool-cry:

--- app.js
+++ app.js
@@ -12,8 +12,8 @@ const sweepButton = document.getElementById('sweep');
 
 /* State */
 let player = {
-    hp: 20,
-    type: roach,
+    hp: 0,
+    // type: roach,
 };
 
 let result = '';
@@ -65,7 +65,27 @@ const oppHit = [0,0,0,0,1,1,1,2,2];
 const oppType = [ant, ant, ant, rat, rat, bloodBug, bloodBug, crab, crab, crab, scorpion, scorpion];
 
 /* Events */
+function displayResult() {
+    results.textContent = result;
+}
 
+function displayScoreboard() {
+    scoreboard.textContent = "You've squashed ${squashed} opponents. Pam will come back soon!";
+}
+
+function displayRoachy() {
+    roachyHP.textContent = Math.max(0, player.hp);
+    if (player.hp < 1) {
+        roachyPic.src = 'assets/deadRoachy.png'
+    } else {
+        roachyPic.src = 'assets/roachy.png';
+    }
+}
+
+function displayOpponents() {
+    oppList.innerHTML = '';
+
+}
 /* Display Functions */
-
+displayRoachy();
 // (don't forget to call any display functions you want to run on page load!)
